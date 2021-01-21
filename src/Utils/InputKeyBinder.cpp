f933237c9e272cbe384a88f9e5c04a48172ec64e
Lukasz Buczynski <pirogronian0@gmail.com>
2021-01-21 22:45:10 +0100

    Input key binding helper utility class and related.

---
+++ src/Utils/InputKeyBinder.cpp
@@ -0,0 +1,68 @@
+
+#include <Utils/VectorHelper.h>
+#include "InputKeyBinder.h"
+
+int IOSP::InputKeyBinder::actionKey(int action) const
+{
+    if (!isIndexValid(m_actionKeys, action))  return 0;
+    return m_actionKeys[action];
+}
+
+bool IOSP::InputKeyBinder::actionShift(int action) const
+{
+    if (!isIndexValid(m_actionShift, action))  return false;
+    return m_actionShift[action];
+}
+
+bool IOSP::InputKeyBinder::actionCtrl(int action) const
+{
+    if (!isIndexValid(m_actionCtrl, action))  return false;
+    return m_actionCtrl[action];
+}
+
+int IOSP::InputKeyBinder::keyVariantAction(int key, bool shift, bool ctrl) const
+{
+    auto itv = m_keyActions.find(key);
+    if (itv == m_keyActions.end())  return 0;
+    return itv->second.access(shift, ctrl);
+}
+
+void IOSP::InputKeyBinder::bind(int action, int key, bool shift, bool ctrl)
+{
+    IOSP::ensureSize(m_actionKeys, action, 0);
+    m_actionKeys.at(action) = key;
+    IOSP::ensureSize(m_actionShift, action, false);
+    m_actionShift.at(action) = shift;
+    IOSP::ensureSize(m_actionCtrl, action, false);
+    m_actionCtrl.at(action) = ctrl;
+    auto& kv = m_keyActions[key];
+    kv.access(shift, ctrl) = action;
+}
+
+void IOSP::InputKeyBinder::unbindKey(int key, bool shift, bool ctrl)
+{
+    auto itv = m_keyActions.find(key);
+    if (itv == m_keyActions.end())  return;
+    int action = itv->second.access(shift, ctrl);
+    itv->second.access(shift, ctrl) = 0;
+    if (m_actionKeys.size() > action)
+        m_actionKeys.at(action) = 0;
+    if (m_actionShift.size() > action)
+        m_actionShift.at(action) = 0;
+    if (m_actionCtrl.size() > action)
+        m_actionCtrl.at(action) = 0;
+}
+
+void IOSP::InputKeyBinder::unbindAction(int action)
+{
+    int key = actionKey(action);
+    bool shift = actionShift(action);
+    bool ctrl = actionCtrl(action);
+    if (isIndexValid(m_actionKeys, action)) m_actionKeys[action] = 0;
+    if (isIndexValid(m_actionShift, action)) m_actionShift[action] = false;
+    if (isIndexValid(m_actionCtrl, action)) m_actionCtrl[action] = false;
+    auto itv = m_keyActions.find(key);
+    if (itv == m_keyActions.end())  return;
+    itv->second.access(shift, ctrl) = 0;
+    if (itv->second.count() == 0)  m_keyActions.erase(itv);
+}
---
+++ src/Utils/InputKeyBinder.h
@@ -0,0 +1,56 @@
+
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+
+namespace IOSP
+{
+    class InputKeyBinder
+    {
+    public:
+        struct KeyActionVariant
+        {
+            int m_basic{0};
+            int m_shift{0};
+            int m_ctrl{0};
+            int m_shift_ctrl{0};
+            int& access(bool shift = false, bool ctrl = false)
+            {
+                if (!shift && !ctrl)  return m_basic;
+                if (shift && !ctrl)  return m_shift;
+                if (!shift && ctrl)  return m_ctrl;
+                return m_shift_ctrl;
+            }
+            const int& access(bool shift = false, bool ctrl = false) const
+            {
+                if (!shift && !ctrl)  return m_basic;
+                if (shift && !ctrl)  return m_shift;
+                if (!shift && ctrl)  return m_ctrl;
+                return m_shift_ctrl;
+            }
+            std::size_t count() const
+            {
+                std::size_t c = 0;
+                if (m_basic > 0) ++c;
+                if (m_shift > 0) ++c;
+                if (m_ctrl > 0) ++c;
+                if (m_shift_ctrl > 0) ++c;
+                return c;
+            }
+        };
+    protected:
+        std::vector<int> m_actionKeys{0, 0};
+        std::vector<bool> m_actionShift{0, false};
+        std::vector<bool> m_actionCtrl{0, false};
+        std::unordered_map<int, KeyActionVariant> m_keyActions;
+    public:
+        int actionKey(int) const;
+        bool actionShift(int) const;
+        bool actionCtrl(int) const;
+        int keyVariantAction(int key, bool shift = false, bool ctrl = false) const;
+        void bind(int action, int key, bool shift = false, bool ctrl = false);
+        void unbindKey(int key, bool shift = false, bool ctrl = false);
+        void unbindAction(int action);
+    };
+}
