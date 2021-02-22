89dbd3bb7f89cd6fd300e291bdbf5c73a89c6a08
Lukasz Buczynski <pirogronian0@gmail.com>
2021-02-22 21:06:20 +0100

    Add Bullet triangle shape from mesh creation.

--- src/Utils/BulletShapes.cpp
+++ src/Utils/BulletShapes.cpp
@@ -41,3 +41,60 @@ void IOSP::ConvexHullAddVertices(
     for(int i = 0; i < mbNum; i++)
         ConvexHullAddVertices(cshape, mesh->getMeshBuffer(i));
 }
+
+void IOSP::addTriangles(
+    btTriangleMesh *mesh,
+    const scene::IMeshBuffer *mbuf,
+    const core::vector3df& scale)
+{
+    const u32 vn = mbuf->getVertexCount();
+    const u16 *inds = mbuf->getIndices();
+    const u32 in = mbuf->getIndexCount();
+    btVector3 triangle[3];
+//     std::printf("Vcount: %i\n", vn);
+
+    switch(mbuf->getVertexType())
+    {
+        case video::EVT_STANDARD:
+        {
+//             std::puts("Vtype: S3DVertex");
+            auto vs = (video::S3DVertex*)mbuf->getVertices();
+            for (int i = 0; i < in; i+=3)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    auto index = inds[i + j];
+                    if (index > vn) continue;
+                    triangle[j] = toBullet(vs[index].Pos * scale);
+                }
+                mesh->addTriangle(triangle[0], triangle [1], triangle[2]);
+            }
+        }
+        break;
+        case video::EVT_2TCOORDS:
+        {
+//             std::puts("Vtype: S3DVertex2TCoords");
+            auto vs = (video::S3DVertex2TCoords*)mbuf->getVertices();
+            for (int i = 0; i < in; i+=3)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    auto index = inds[i + j];
+                    if (index > vn) continue;
+                    triangle[j] = toBullet(vs[index].Pos * scale);
+                }
+                mesh->addTriangle(triangle[0], triangle [1], triangle[2]);
+            }
+        }
+    }
+}
+
+void IOSP::addTriangles(
+    btTriangleMesh *bmesh,
+    const irr::scene::IMesh *imesh,
+    const core::vector3df& scale)
+{
+    int mbNum = imesh->getMeshBufferCount();
+        for(int i = 0; i < mbNum; i++)
+            addTriangles(bmesh, imesh->getMeshBuffer(i), scale);
+}
--- src/Utils/BulletShapes.h
+++ src/Utils/BulletShapes.h
@@ -48,4 +48,43 @@ namespace IOSP
         ConvexHullAddVertices(ret, mnode);
         return ret;
     }
+
+    void addTriangles(
+        btTriangleMesh*,
+        const irr::scene::IMeshBuffer *,
+        const irr::core::vector3df& = irr::core::vector3df(1, 1, 1));
+
+    void addTriangles(
+        btTriangleMesh*,
+        const irr::scene::IMesh *,
+        const irr::core::vector3df& = irr::core::vector3df(1, 1, 1));
+
+    inline btBvhTriangleMeshShape *createTriangleShape(btTriangleMesh *tm)
+    {
+        return new btBvhTriangleMeshShape(tm, false, true);
+    }
+
+    inline btBvhTriangleMeshShape *createTriangleShape(
+        const irr::scene::IMeshBuffer *mb,
+        const irr::core::vector3df& scale = irr::core::vector3df(1, 1, 1))
+    {
+        auto tm = new btTriangleMesh();
+        addTriangles(tm, mb, scale);
+        return createTriangleShape(tm);
+    }
+
+    inline btBvhTriangleMeshShape *createTriangleShape(
+        const irr::scene::IMesh *m,
+        const irr::core::vector3df& scale = irr::core::vector3df(1, 1, 1))
+    {
+        auto tm = new btTriangleMesh();
+        addTriangles(tm, m, scale);
+        return createTriangleShape(tm);
+    }
+
+    inline btBvhTriangleMeshShape *createTriangleShape(const irr::scene::IMeshSceneNode *node)
+    {
+        return createTriangleShape(
+            (const irr::scene::IMesh*)((irr::scene::IMeshSceneNode *)node)->getMesh(), node->getScale());
+    }
 }
